9c3bbb37a184fc107ca4e256de3d15aeaf16550d
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-10-13 19:56:33 +0200

    Move function to named_range module as that's where the tests are.

--- openpyxl2/reader/excel.py
+++ openpyxl2/reader/excel.py
@@ -34,11 +34,11 @@ from openpyxl2.xml.constants import (
 
 from openpyxl2.workbook import Workbook, DocumentProperties
 from openpyxl2.workbook.names.external import detect_external_links
+from openpyxl2.workbook.names.named_range import read_named_ranges
 from openpyxl2.reader.strings import read_string_table
 from openpyxl2.reader.style import read_style_table
 from openpyxl2.reader.workbook import (
     read_content_types,
-    read_named_ranges,
     read_properties_core,
     read_excel_base_date,
     detect_worksheets,
