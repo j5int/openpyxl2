f79a39af1991282c59f8ca7f168712681cefb60e
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-04-08 12:39:38 +0200

    Preliminary implementation of nested descriptors for the most common use case.

---
+++ openpyxl2/descriptors/nested.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+#copyright openpyxl 2010-2015
+
+"""
+Generic serialisable classes
+"""
+from .base import Typed, Descriptor
+from openpyxl2.compat import safe_string
+from openpyxl2.xml.functions import Element
+
+
+class Value(Descriptor):
+    """
+    Nested tag storing the value on the 'val' attribute
+    """
+
+    nested = True
+
+    def __set__(self, instance, value):
+        if hasattr(value, "findall"):
+            value = self.from_tree(value)
+        super(Value, self).__set__(instance, value)
+
+
+    def from_tree(self, node):
+        return node.get("val")
+
+
+    @staticmethod
+    def to_tree(tagname=None, value=None):
+        value = safe_string(value)
+        return Element(tagname, val=value)
+
+
+class Text(Value):
+    """
+    Represents any nested tag with the value as the contents of the tag
+    """
+
+
+    def from_tree(self, node):
+        return node.text
+
+
+    @staticmethod
+    def to_tree(tagname=None, value=None):
+        el = Element(tagname)
+        el.text = safe_string(value)
+        return el
---
+++ openpyxl2/descriptors/tests/test_nested.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import
+#copyright openpyxl 2010-2015
+
+from openpyxl2.xml.functions import tostring, fromstring
+from openpyxl2.tests.helper import compare_xml
+from ..serialisable import Serialisable
+
+
+import pytest
+
+@pytest.fixture
+def NestedValue():
+    from ..nested import Value
+
+    class Simple(Serialisable):
+
+        size = Value(expected_type=int)
+
+        def __init__(self, size):
+            self.size = size
+
+    return Simple
+
+
+class TestValue:
+
+    def test_to_tree(self, NestedValue):
+
+        simple = NestedValue(4)
+
+        assert simple.size == 4
+        xml = tostring(NestedValue.size.to_tree("size", simple.size))
+        expected = """
+        <size val="4"></size>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_tree(self, NestedValue):
+
+        xml = """
+            <size val="4"></size>
+            """
+        node = fromstring(xml)
+        simple = NestedValue(size=node)
+        assert simple.size == "4"
+
+
+@pytest.fixture
+def NestedText():
+
+    from ..nested import Text
+
+    class Simple(Serialisable):
+
+        coord = Text(expected_type=int)
+
+        def __init__(self, coord):
+            self.coord = coord
+
+    return Simple
+
+
+class TestText:
+
+    def test_to_tree(self, NestedText):
+
+        simple = NestedText(4)
+
+        assert simple.coord == 4
+        xml = tostring(NestedText.coord.to_tree("coord", simple.coord))
+        expected = """
+        <coord>4</coord>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_tree(self, NestedText):
+        xml = """
+            <coord>4</coord>
+            """
+        node = fromstring(xml)
+
+        simple = NestedText(node)
+        assert simple.coord == "4"
