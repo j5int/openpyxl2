deb510d5c37d2d2793d6615b64e61162bbe2c681
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-04-18 19:11:34 +0200

    Stub out more tests.

--- openpyxl2/chart/tests/test_bar_chart.py
+++ openpyxl2/chart/tests/test_bar_chart.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import pytest
 
-from openpyxl2.xml.functions import tostring
+from openpyxl2.xml.functions import tostring, fromstring
 from openpyxl2.tests.helper import compare_xml
 
 
@@ -12,17 +12,33 @@ def BarChart():
     return BarChart
 
 
-def test_ctor(BarChart):
-    bc = BarChart()
-    xml = tostring(bc.to_tree())
-    expected = """
-    <barChart>
-      <barDir val="col" />
-      <grouping val="clustered" />
+class TestBarChart:
 
-      <axId val="60871424" />
-      <axId val="60873344" />
-    </barChart>
-    """
-    diff = compare_xml(xml, expected)
-    assert diff is None, diff
+
+    def test_ctor(self, BarChart):
+        bc = BarChart()
+        xml = tostring(bc.to_tree())
+        expected = """
+        <barChart>
+          <barDir val="col" />
+          <grouping val="clustered" />
+
+          <axId val="60871424" />
+          <axId val="60873344" />
+        </barChart>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_tree(self, BarChart):
+        xml = """
+        <root />
+        """
+        node = fromstring(xml)
+        bc = BarChart.from_tree(node)
+
+
+    def test_serialise(self, BarChart):
+
+        bc = BarChart()
