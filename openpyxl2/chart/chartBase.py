0427497fc74dc8f9130310fe0fe6bf361d3370c9
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-04-19 14:36:00 +0200

    Add more required classes.

--- openpyxl2/chart/chartBase.py
+++ openpyxl2/chart/chartBase.py
@@ -7,10 +7,13 @@ from openpyxl2.descriptors import (
     Bool,
     Float,
     Typed,
-    MinMax
+    MinMax,
+    Alias,
+    String,
+    Integer
 )
 from openpyxl2.descriptors.excel import ExtensionList
-
+from openpyxl2.descriptors.nested import NestedString, NestedText
 
 from .shapes import ShapeProperties
 
@@ -19,9 +22,62 @@ class AxDataSource(Serialisable):
 
     pass
 
+
+class NumVal(Serialisable):
+
+    idx = Integer()
+    formatCode = NestedString(allow_none=True)
+    v = NestedText(allow_none=True)
+
+    def __init__(self,
+                 idx=None,
+                 formatCode=None,
+                 v=None,
+                ):
+        self.idx = idx
+        self.formatCode = formatCode
+        self.v = v
+
+
+class NumData(Serialisable):
+
+    formatCode = Typed(expected_type=String(), allow_none=True)
+    ptCount = Integer(allow_none=True)
+    pt = Typed(expected_type=NumVal, allow_none=True)
+    extLst = Typed(expected_type=ExtensionList, allow_none=True)
+
+    def __init__(self,
+                 formatCode=None,
+                 ptCount=None,
+                 pt=None,
+                 extLst=None,
+                ):
+        self.formatCode = formatCode
+        self.ptCount = ptCount
+        self.pt = pt
+
+
+class NumRef(Serialisable):
+
+    f = NestedString()
+    ref = Alias('f')
+    numCache = Typed(expected_type=NumData, allow_none=True)
+    extLst = Typed(expected_type=ExtensionList, allow_none=True)
+
+    def __init__(self,
+                 f=None,
+                 numCache=None,
+                 extLst=None,
+                ):
+        self.f = f
+
+
 class NumDataSource(Serialisable):
 
-    pass
+    numRef = Typed(expected_type=NumRef)
+
+    def __init__(self, numRef):
+        self.numRef = numRef
 
 
 class GapAmount(Serialisable):
