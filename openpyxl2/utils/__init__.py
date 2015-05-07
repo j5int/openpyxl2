680f1f67cdecdf13b849a517055f8c69955220b8
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-05-07 16:33:52 +0200

    Function for converting sheet ranges to sheetnames and range boundaries for use when normalising.

--- openpyxl2/utils/__init__.py
+++ openpyxl2/utils/__init__.py
@@ -13,16 +13,16 @@ from openpyxl2.utils.exceptions import CellCoordinatesException
 
 # constants
 COORD_RE = re.compile('^[$]?([A-Z]+)[$]?(\d+)$')
-ABSOLUTE_RE = re.compile(
-'''^[$]?
-(?P<min_col>[A-Z]+)
-[$]?
-(?P<min_row>\d+)
-(:[$]?
-(?P<max_col>[A-Z]+)
-[$]?
-(?P<max_row>\d+))?$''',
-re.VERBOSE)
+RANGE_EXPR = """
+[$]?(?P<min_col>[A-Z]+)
+[$]?(?P<min_row>\d+)
+(:[$]?(?P<max_col>[A-Z]+)
+[$]?(?P<max_row>\d+))?
+"""
+ABSOLUTE_RE = re.compile('^' + RANGE_EXPR +'$', re.VERBOSE)
+SHEETRANGE_RE = re.compile("""
+^(('(?P<quoted>([^']|'')*)')|(?P<notquoted>[^']*))!
+(?P<cells>{0})$""".format(RANGE_EXPR), re.VERBOSE)
 
 
 def get_column_interval(start, end):
@@ -149,3 +149,11 @@ def coordinate_to_tuple(coordinate):
     """
     col, row = coordinate_from_string(coordinate)
     return row, _COL_STRING_CACHE[col]
+
+
+def range_to_tuple(range_string):
+    m = SHEETRANGE_RE.match(range_string)
+    sheetname = m.group("quoted") or m.group("notquoted")
+    cells = m.group("cells")
+    boundaries = range_boundaries(cells)
+    return sheetname, boundaries
--- openpyxl2/utils/tests/test_utils.py
+++ openpyxl2/utils/tests/test_utils.py
@@ -93,3 +93,15 @@ def test_column_letter(value, expected):
 def test_coordinate_tuple():
     from .. import coordinate_to_tuple
     assert coordinate_to_tuple("D15") == (15, 4)
+
+
+
+@pytest.mark.parametrize("range_string, sheetname, boundaries",
+                         [
+                        ("Sheet1!$A$1:$A$12", "Sheet1", (1,1, 1,12)),
+                        ("'My Sheet'!A1:E6", "My Sheet", (1,1, 5,6)),
+                         ]
+                         )
+def test_range_to_tuple(range_string, sheetname, boundaries):
+    from .. import range_to_tuple
+    assert range_to_tuple(range_string) == (sheetname, boundaries)
