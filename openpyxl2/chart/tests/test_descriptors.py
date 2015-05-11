68d347643beca275e1240241aad3a49fcd6f1f0a
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-05-11 12:19:20 +0200

    Add test for descriptors.

---
+++ openpyxl2/chart/tests/test_descriptors.py
@@ -0,0 +1,19 @@
+
+from __future__ import absolute_import
+
+import pytest
+
+from openpyxl2.xml.functions import fromstring, tostring
+from openpyxl2.tests.helper import compare_xml
+
+
+def test_color_descriptor():
+    from ..colors import ColorChoiceDescriptor
+
+    class DummyStyle(object):
+
+        value = ColorChoiceDescriptor('value')
+
+    style = DummyStyle()
+    style.value = "efefef"
+    style.value.RGB == "efefef"
