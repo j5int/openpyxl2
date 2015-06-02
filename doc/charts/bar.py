c3b3f86ddd41561a9a977bebcdb6e985ec08d656
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-06-02 12:13:40 +0200

    How to make bar charts.

--- doc/charts/bar.py
+++ doc/charts/bar.py
@@ -1,6 +1,5 @@
 from openpyxl2 import Workbook
 from openpyxl2.chart import BarChart, Series, Reference
-from openpyxl2.chart.title import title_maker
 
 wb = Workbook(write_only=True)
 ws = wb.create_sheet()
@@ -24,8 +23,8 @@ chart1 = BarChart()
 chart1.type = "bar"
 chart1.style = 11
 chart1.title = "Bar chart"
-chart1.y_axis.title = title_maker('Test number')
-chart1.x_axis.title = title_maker('Sample length (mm)')
+chart1.y_axis.title = 'Test number'
+chart1.x_axis.title = 'Sample length (mm)'
 
 data = Reference(ws, min_col=2, min_row=1, max_row=7, max_col=3)
 cats = Reference(ws, min_col=1, min_row=2, max_row=7)
@@ -36,13 +35,13 @@ ws.add_chart(chart1, "D2")
 
 
 chart2 = BarChart()
-chart2.type = "bar"
+chart2.type = "col"
 chart2.style = 12
 chart2.grouping = "stacked"
 chart2.overlap = 100
 chart2.title = 'Stacked Chart'
-chart2.y_axis.title = title_maker('Test number')
-chart2.x_axis.title = title_maker('Sample length (mm)')
+chart2.y_axis.title = 'Test number'
+chart2.x_axis.title = 'Sample length (mm)'
 chart2.add_data(data, titles_from_data=True)
 chart2.set_categories(cats)
 ws.add_chart(chart2, "D18")
@@ -54,8 +53,8 @@ chart3.style = 13
 chart3.grouping = "percentStacked"
 chart3.overlap = 100
 chart3.title = 'Percent Stacked Chart'
-chart3.y_axis.title = title_maker('Test number')
-chart3.x_axis.title = title_maker('Sample length (mm)')
+chart3.y_axis.title = 'Test number'
+chart3.x_axis.title = 'Sample length (mm)'
 chart3.add_data(data, titles_from_data=True)
 chart3.set_categories(cats)
 ws.add_chart(chart3, "D34")
---
+++ doc/charts/bar.rst
@@ -0,0 +1,12 @@
+Bar and Column Charts
+=====================
+
+In bar charts values are plotted as either horizontal bars or vertical columns.
+
+.. literalinclude:: bar.py
+
+
+This will produce three charts illustrating the various possibilities. Note that when the bars are horizontal the x and y axes are revesed.
+
+.. image:: bar.png
+   :alt: "Sample bar chart"
