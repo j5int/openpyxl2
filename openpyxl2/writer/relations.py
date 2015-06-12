24f24e3607fe325304635c893a1bbd9026948d27
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-06-12 15:19:22 +0200

    Code cleanup and use absolute URLs

--- openpyxl2/writer/relations.py
+++ openpyxl2/writer/relations.py
@@ -2,12 +2,9 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2015 openpyxl
 
 
-from openpyxl2.xml.functions import Element, SubElement
+from openpyxl2.xml.functions import Element
 from openpyxl2.xml.constants import (
-    COMMENTS_NS,
     PKG_REL_NS,
-    REL_NS,
-    VML_NS,
 )
 from openpyxl2.packaging.relationship import Relationship
 
@@ -20,17 +17,16 @@ def write_rels(worksheet, comments_id=None, vba_controls_id=None):
     if worksheet._comment_count > 0:
 
         rel = Relationship(type="comments", id="comments",
-                           target='../comments%s.xml' % comments_id)
+                           target='/comments%s.xml' % comments_id)
         rels.append(rel)
 
-        rel = Relationship("type", target='../drawings/commentsDrawing%s.vml' % comments_id, id="commentsvml")
-        rel.type = VML_NS
+        rel = Relationship(type="vmlDrawing", id="commentsvml",
+                           target='/drawings/commentsDrawing%s.vml' % comments_id)
         rels.append(rel)
 
     if worksheet.vba_controls is not None:
-        rel = Relationship("type", target='../drawings/vmlDrawing%s.vml' %
-                           vba_controls_id, id=worksheet.vba_controls)
-        rel.type = VML_NS
+        rel = Relationship("vmlDrawing", id=worksheet.vba_controls,
+                           target='/drawings/vmlDrawing%s.vml' % vba_controls_id)
         rels.append(rel)
 
     for idx, rel in enumerate(rels, 1):
