d08d024894f29a702c48935cad6869b57c008f09
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-06-15 11:16:44 +0200

    Add unit test for when there are both comments and vba

--- openpyxl2/writer/tests/test_relations.py
+++ openpyxl2/writer/tests/test_relations.py
@@ -64,3 +64,18 @@ class TestRels:
         xml = tostring(writer(ws))
         diff = compare_xml(xml, expected)
         assert diff is None, diff
+
+
+    def test_vba_and_comments(self, writer):
+        ws = Worksheet()
+        ws.vba_controls = "vba"
+        ws._comment_count = 1
+        expected = """
+        <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+          <Relationship Id="vba" Target="/xl/drawings/vmlDrawing1.vml" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/vmlDrawing"/>
+          <Relationship Id="comments" Target="/xl/comments1.xml" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/comments" />
+        </Relationships>
+            """
+        xml = tostring(writer(ws, vba_controls_id=1, comments_id=1))
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
