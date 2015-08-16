31d004a2dca4b44512e8363e58adc0846206efdb
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-08-16 15:33:23 +0200

    Remove import.

--- openpyxl2/styles/styleable.py
+++ openpyxl2/styles/styleable.py
@@ -10,7 +10,6 @@ from openpyxl2.xml.functions import Element
 from openpyxl2.utils.indexed_list import IndexedList
 from .numbers import BUILTIN_FORMATS, BUILTIN_FORMATS_REVERSE
 from .proxy import StyleProxy
-from .style import StyleId
 from . import Style
 
 
