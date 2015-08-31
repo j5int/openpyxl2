3cc898da6ae43b2524912aee7641ab52ec0b3900
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-08-31 20:08:16 +0200

    Naive test for generating a simple class.

--- openpyxl2/develop/tests/test_classify.py
+++ openpyxl2/develop/tests/test_classify.py
@@ -21,3 +21,28 @@ def test_element_group(schema):
     from ..classify import get_element_group
     els = get_element_group(schema, "EG_FillProperties")
     assert [el.get('name') for el in els] == ['noFill', 'solidFill', 'gradFill', 'blipFill', 'pattFill', 'grpFill']
+
+
+def test_class_no_deps(schema):
+    from ..classify import classify
+    cls = classify("CT_FileRecoveryPr")
+    assert cls[0] == """
+
+class FileRecoveryPr(Serialisable):
+
+    autoRecover = Bool(allow_none=True)
+    crashSave = Bool(allow_none=True)
+    dataExtractLoad = Bool(allow_none=True)
+    repairLoad = Bool(allow_none=True)
+
+    def __init__(self,
+                 autoRecover=None,
+                 crashSave=None,
+                 dataExtractLoad=None,
+                 repairLoad=None,
+                ):
+        self.autoRecover = autoRecover
+        self.crashSave = crashSave
+        self.dataExtractLoad = dataExtractLoad
+        self.repairLoad = repairLoad
+"""
