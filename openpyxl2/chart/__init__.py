1ac1cff985e992b9cdeaa0c715a7e31b901ddeff
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-09-23 10:18:34 +0200

    Export RadarChart and correct definition of series.

--- openpyxl2/chart/__init__.py
+++ openpyxl2/chart/__init__.py
@@ -5,7 +5,13 @@ from .area_chart import AreaChart, AreaChart3D
 from .bar_chart import BarChart, BarChart3D
 from .bubble_chart import BubbleChart
 from .line_chart import LineChart, LineChart3D
-from .pie_chart import PieChart, PieChart3D, DoughnutChart, ProjectedPieChart
+from .pie_chart import (
+    PieChart,
+    PieChart3D,
+    DoughnutChart,
+    ProjectedPieChart
+)
+from .radar_chart import RadarChart
 from .scatter_chart import ScatterChart
 from .stock_chart import StockChart
 from .surface_chart import SurfaceChart, SurfaceChart3D
