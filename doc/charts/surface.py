49dd056f587899d99bc6b2fe7b84efa849b417fb
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-09-25 19:28:51 +0200

    Initial version of surface chart docs.

---
+++ doc/charts/surface.py
@@ -0,0 +1,38 @@
+from openpyxl2 import Workbook
+from openpyxl2.chart import (
+    SurfaceChart,
+    Reference,
+    Series,
+)
+
+wb = Workbook()
+ws = wb.active
+
+data = [
+    [None, 10, 20, 30, 40, 50,],
+    [0.1, 15, 65, 105, 65, 15,],
+    [0.2, 35, 105, 170, 105, 35,],
+    [0.3, 55, 135, 215, 135, 55,],
+    [0.4, 75, 155, 240, 155, 75,],
+    [0.5, 80, 190, 245, 190, 80,],
+    [0.6, 75, 155, 240, 155, 75,],
+    [0.7, 55, 135, 215, 135, 55,],
+    [0.8, 35, 105, 170, 105, 35,],
+    [0.9, 15, 65, 105, 65, 15],
+]
+
+for row in data:
+    ws.append(row)
+
+
+
+c1 = SurfaceChart()
+ref = Reference(ws, min_col=2, max_col=5, min_row=1, max_row=10)
+labels = Reference(ws, min_col=1, min_row=2, max_row=10)
+c1.add_data(ref, titles_from_data=True)
+c1.set_categories(labels)
+c1.title = "Surface Chart"
+
+ws.add_chart(c1, "A12")
+
+wb.save("surface.xlsx")
