d9f69bdad4fb139498be9b33da9096e000436bdc
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-09-26 10:24:33 +0200

    Add examples for 3D charts.

--- doc/charts/surface.py
+++ doc/charts/surface.py
@@ -1,6 +1,7 @@
 from openpyxl2 import Workbook
 from openpyxl2.chart import (
     SurfaceChart,
+    SurfaceChart3D,
     Reference,
     Series,
 )
@@ -26,14 +27,36 @@ for row in data:
     ws.append(row)
 
 
-
 c1 = SurfaceChart()
 ref = Reference(ws, min_col=2, max_col=6, min_row=1, max_row=10)
 labels = Reference(ws, min_col=1, min_row=2, max_row=10)
 c1.add_data(ref, titles_from_data=True)
 c1.set_categories(labels)
-c1.title = "Surface Chart"
+c1.title = "Contour"
 
 ws.add_chart(c1, "A12")
+
+from copy import deepcopy
+
+# wireframe
+c2 = deepcopy(c1)
+c2.wireframe = True
+c2.title = "2D Wireframe"
+
+ws.add_chart(c2, "G12")
+
+# 3D Surface
+c3 = SurfaceChart3D()
+c3.add_data(ref, titles_from_data=True)
+c3.set_categories(labels)
+c3.title = "Surface"
+
+ws.add_chart(c3, "A29")
+
+c4 = deepcopy(c3)
+c4.wireframe = True
+c4.title = "3D Wireframe"
+
+ws.add_chart(c4, "G29")
 
 wb.save("surface.xlsx")
