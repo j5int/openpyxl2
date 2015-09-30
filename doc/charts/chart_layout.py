9fe874ed360713a500effad0719c572253a4f4d7
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-09-30 14:12:25 +0200

    Update layout docs.

--- doc/charts/chart_layout.py
+++ doc/charts/chart_layout.py
@@ -18,33 +18,61 @@ rows = [
 for row in rows:
     ws.append(row)
 
-ch = ScatterChart()
-ch.scatterStyle = "marker"
-ch.title = "Scatter Chart with Layout"
-ch.style = 13
-ch.x_axis.title = 'Size'
-ch.y_axis.title = 'Percentage'
-
+ch1 = ScatterChart()
 xvalues = Reference(ws, min_col=1, min_row=2, max_row=7)
 for i in range(2, 4):
     values = Reference(ws, min_col=i, min_row=1, max_row=7)
     series = Series(values, xvalues, title_from_data=True)
-    ch.series.append(series)
-    
-# Set layout of the legend    
-ch.legend.layout=Layout(manualLayout=ManualLayout(yMode='edge', xMode='edge',x=0,y=0.9, h=0.1, w=1))
-# x is the lateral position counted from the left in relative coordinates [0,1]
-# y is the vertical position counted from the left in relative coordinates [0,1]
-# h is the height of the box 
-# w is the width of the box
+    ch1.series.append(series)
 
-# Set layout of the plot area
-ch.plot_area.layout=Layout(manualLayout=ManualLayout(yMode='edge', xMode='edge',x=0.1,y=0.15, h=0.65, w=0.85))
-# x is the lateral position counted from the left in relative coordinates [0,1]
-# y is the vertical position counted from the left in relative coordinates [0,1]
-# h is the height of the box 
-# w is the width of the box
 
-ws.add_chart(ch, "B10")
+ch1.title = "Default layout"
+ch1.style = 13
+ch1.x_axis.title = 'Size'
+ch1.y_axis.title = 'Percentage'
+ch1.legend.position = 'r'
+
+ws.add_chart(ch1, "B10")
+
+from copy import deepcopy
+
+# Half-size chart, bottom right
+ch2 = deepcopy(ch1)
+ch2.title = "Manual chart layout"
+ch2.legend.position = "tr"
+ch2.layout=Layout(
+    manualLayout=ManualLayout(
+        x=0.25, y=0.25,
+        h=0.5, w=0.5,
+    )
+)
+ws.add_chart(ch2, "H10")
+
+# Half-size chart, centred
+ch3 = deepcopy(ch1)
+ch3.layout = Layout(
+    ManualLayout(
+    x=0.25, y=0.25,
+    h=0.5, w=0.5,
+    xMode="edge",
+    yMode="edge",
+    )
+)
+ch3.title = "Manual chart layout, edge mode"
+ws.add_chart(ch3, "B27")
+
+# Manually position the legend bottom left
+ch4 = deepcopy(ch1)
+ch4.title = "Manual legend layout"
+ch4.legend.layout = Layout(
+    manualLayout=ManualLayout(
+        yMode='edge',
+        xMode='edge',
+        x=0, y=0.9,
+        h=0.1, w=0.5
+    )
+)
+
+ws.add_chart(ch4, "H27")
 
 wb.save("chart_layout.xlsx")
