5faa83b327c6fe85107723f350e26633e6589476
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-10-05 20:18:14 +0200

    Remove imports into module scope as nothing is required for client code.

--- openpyxl2/reader/__init__.py
+++ openpyxl2/reader/__init__.py
@@ -1,10 +1,2 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2015 openpyxl
-
-"""Imports for the openpyxl.reader namespace."""
-
-# package imports
-from openpyxl2.reader import excel
-from openpyxl2.reader import strings
-from openpyxl2.reader import workbook
-from openpyxl2.reader import worksheet
