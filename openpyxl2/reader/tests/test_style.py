4444f52e8e77fb40a9b5ad7947a5864381532685
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-10-05 20:14:29 +0200

    Remove tests now covered by module unit tests (probably need to add some).

--- openpyxl2/reader/tests/test_style.py
+++ openpyxl2/reader/tests/test_style.py
@@ -3,19 +3,8 @@ from __future__ import absolute_import
 
 import pytest
 
-import codecs
-from io import BytesIO
-from zipfile import ZipFile
-
-# package imports
 from openpyxl2.reader.excel import load_workbook
-from openpyxl2.utils.indexed_list import IndexedList
-from openpyxl2.styles.styleable import StyleArray
-from openpyxl2.xml.functions import fromstring
-
 from openpyxl2.styles import (
-    borders,
-    numbers,
     Color,
     Font,
     PatternFill,
@@ -23,78 +12,7 @@ from openpyxl2.styles import (
     Border,
     Side,
     Alignment,
-    Protection,
 )
-from openpyxl2.xml.functions import Element
-
-
-@pytest.fixture
-def StyleReader():
-    from ..style import SharedStylesParser
-    return SharedStylesParser
-
-
-@pytest.mark.parametrize("value, expected",
-                         [
-                         ('f', False),
-                         ('0', False),
-                         ('false', False),
-                         ('1', True),
-                         ('t', True),
-                         ('true', True),
-                         ('anyvalue', True),
-                         ])
-def test_bool_attrib(value, expected):
-    from .. style import bool_attrib
-    el = Element("root", value=value)
-    assert bool_attrib(el, "value") is expected
-
-
-def test_read_complex_style_mappings(datadir, StyleReader):
-    datadir.chdir()
-    with open("complex-styles.xml") as content:
-        reader = StyleReader(content.read())
-    reader.parse()
-    styles  = reader.cell_styles
-    assert len(styles) == 29
-    assert styles[-1] == StyleArray([6,5,0,0,0,0,0,0,0])
-
-
-def test_read_complex_fonts(datadir, StyleReader):
-    from openpyxl2.styles import Font
-    datadir.chdir()
-    with open("complex-styles.xml") as content:
-        reader = StyleReader(content.read())
-    fonts = list(reader.parse_fonts())
-    assert len(fonts) == 8
-    assert fonts[7] == Font(size=12, color=Color(theme=9), name="Calibri", scheme="minor")
-
-
-def test_read_complex_fills(datadir, StyleReader):
-    datadir.chdir()
-    with open("complex-styles.xml") as content:
-        reader = StyleReader(content.read())
-    fills = list(reader.parse_fills())
-    assert len(fills) == 6
-
-
-def test_read_complex_borders(datadir, StyleReader):
-    datadir.chdir()
-    with open("complex-styles.xml") as content:
-        reader = StyleReader(content.read())
-    borders = list(reader.parse_borders())
-    assert len(borders) == 7
-
-
-def test_read_simple_style_mappings(datadir, StyleReader):
-    datadir.chdir()
-    with open("simple-styles.xml") as content:
-        reader = StyleReader(content.read())
-    reader.parse()
-    styles  = reader.cell_styles
-    assert len(styles) == 4
-    assert styles[1].numFmtId == 9
-    assert styles[2].numFmtId == 164
 
 
 def test_read_complex_style(datadir):
@@ -103,8 +21,9 @@ def test_read_complex_style(datadir):
     ws = wb.active
     assert ws.column_dimensions['A'].width == 31.1640625
 
-    #assert ws.column_dimensions['I'].font == Font(sz=12.0, color='FF3300FF', scheme='minor')
-    assert ws.column_dimensions['I'].fill == PatternFill(patternType='solid', fgColor='FF006600', bgColor=Color(indexed=64))
+    assert ws.column_dimensions['I'].font == Font(sz=12.0, color='FF3300FF', scheme='minor')
+    assert ws.column_dimensions['I'].fill == PatternFill(patternType='solid',
+                                                         fgColor='FF006600', bgColor=Color(indexed=64))
 
     assert ws['A2'].font == Font(sz=10, name='Arial', color=Color(theme=1))
     assert ws['A3'].font == Font(sz=12, name='Arial', bold=True, color=Color(theme=1))
@@ -139,16 +58,11 @@ def test_read_complex_style(datadir):
         bottom=Side(style='double', color=Color(theme=7)),
     )
 
-    assert ws['A23'].fill == PatternFill(patternType='solid', start_color='FFCCCCFF', end_color=(Color(indexed=64)))
+    assert ws['A23'].fill == PatternFill(patternType='solid',
+                                         start_color='FFCCCCFF', end_color=(Color(indexed=64)))
     assert ws['A23'].border.top == Side(style='mediumDashed', color=Color(theme=6))
 
     assert 'A23:B24' in ws._merged_cells
 
     assert ws['A25'].alignment == Alignment(wrapText=True)
     assert ws['A26'].alignment == Alignment(shrinkToFit=True)
-
-
-def test_no_styles():
-    from .. style import read_style_table
-    archive = ZipFile(BytesIO(), "a")
-    assert read_style_table(archive) is None
