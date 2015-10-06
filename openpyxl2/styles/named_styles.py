44f06a2f1a5cf6f54df1aec4de81f4071ccbdc6c
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-10-06 09:21:44 +0200

    Inherit from HashableObject.

--- openpyxl2/styles/named_styles.py
+++ openpyxl2/styles/named_styles.py
@@ -15,14 +15,15 @@ from openpyxl2.descriptors.excel import ExtensionList
 from openpyxl2.descriptors.serialisable import Serialisable
 
 from .fills import PatternFill, Fill
-from . fonts import Font, DEFAULT_FONT
-from . borders import Border
-from . alignment import Alignment
-from . numbers import NumberFormatDescriptor
-from . protection import Protection
+from .fonts import Font, DEFAULT_FONT
+from .borders import Border
+from .alignment import Alignment
+from .numbers import NumberFormatDescriptor
+from .protection import Protection
+from .hashable import HashableObject
 
 
-class NamedStyle(Strict):
+class NamedStyle(HashableObject):
 
     """
     Named and editable styles
@@ -37,7 +38,8 @@ class NamedStyle(Strict):
     builtinId = Integer(allow_none=True)
     hidden = Bool(allow_none=True)
 
-    __fields__ = ("name", "font", "fill", "border", "number_format", "alignment", "protection")
+    __fields__ = ("name", "font", "fill", "border", "number_format",
+                  "alignment", "protection")
 
     def __init__(self,
                  name="Normal",
@@ -59,36 +61,6 @@ class NamedStyle(Strict):
         self.protection = protection
         self.builtinId = builtinId
         self.hidden = hidden
-
-
-    def _make_key(self):
-        """Use a tuple of fields as the basis for a key"""
-        self._key = hash(tuple(getattr(self, x) for x in self.__fields__))
-
-    def __hash__(self):
-        if not hasattr(self, '_key'):
-            self._make_key()
-        return self._key
-
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            if not hasattr(self, '_key'):
-                self._make_key()
-            if not hasattr(other, '_key'):
-                other._make_key()
-            return self._key == other._key
-
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __repr__(self):
-        pieces = []
-        for k in self.__fields__:
-            value = getattr(self, k)
-            pieces.append('%s=%s' % (k, repr(value)))
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(pieces))
 
 
     def __iter__(self):
