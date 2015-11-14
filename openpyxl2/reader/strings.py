35873baf8ec2cba22f59901e5aa84f33988d1e7b
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-11-14 17:56:59 +0100

    Switch to using an iterative parser.

--- openpyxl2/reader/strings.py
+++ openpyxl2/reader/strings.py
@@ -1,23 +1,27 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2015 openpyxl
 
-"""Read the shared strings table."""
-
 from openpyxl2.cell.text import Text
-
 from openpyxl2.utils.indexed_list import IndexedList
-from openpyxl2.xml.functions import fromstring, safe_iterator
+
+from openpyxl2.xml.functions import iterparse
 from openpyxl2.xml.constants import SHEET_MAIN_NS
+
+from .worksheet import _get_xml_iter
 
 
 def read_string_table(xml_source):
     """Read in all shared strings in the table"""
-    root = fromstring(xml_source)
-    nodes = safe_iterator(root, '{%s}si' % SHEET_MAIN_NS)
     strings = []
-    for node in nodes:
-        text = Text.from_tree(node).content
-        text = text.replace('x005F_', '')
-        strings.append(text)
+    src = _get_xml_iter(xml_source)
+
+    for _, node in iterparse(src):
+        if node.tag == '{%s}si' % SHEET_MAIN_NS:
+
+            text = Text.from_tree(node).content
+            text = text.replace('x005F_', '')
+            strings.append(text)
+
+            node.clear()
 
     return IndexedList(strings)
