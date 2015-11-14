8cecf351a9a358b6ea82ac1778868336e672ef04
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-11-14 17:24:21 +0100

    Backport from 2.4

--- openpyxl2/reader/strings.py
+++ openpyxl2/reader/strings.py
@@ -3,42 +3,21 @@ from __future__ import absolute_import
 
 """Read the shared strings table."""
 
-from openpyxl2.compat import unicode
+from openpyxl2.cell.text import Text
 
-# package imports
 from openpyxl2.utils.indexed_list import IndexedList
 from openpyxl2.xml.functions import fromstring, safe_iterator
-from openpyxl2.xml.constants import SHEET_MAIN_NS, XML_NS
+from openpyxl2.xml.constants import SHEET_MAIN_NS
 
 
 def read_string_table(xml_source):
     """Read in all shared strings in the table"""
-    root = fromstring(text=xml_source)
+    root = fromstring(xml_source)
     nodes = safe_iterator(root, '{%s}si' % SHEET_MAIN_NS)
-    strings = (get_string(node) for node in nodes)
+    strings = []
+    for node in nodes:
+        text = Text.from_tree(node).content
+        text = text.replace('x005F_', '')
+        strings.append(text)
+
     return IndexedList(strings)
-
-
-def get_string(string_index_node):
-    """Read the contents of a specific string index"""
-    rich_nodes = string_index_node.findall('{%s}r' % SHEET_MAIN_NS)
-    if rich_nodes:
-        reconstructed_text = []
-        for rich_node in rich_nodes:
-            partial_text = get_text(rich_node)
-            reconstructed_text.append(partial_text)
-        return unicode(''.join(reconstructed_text))
-    return get_text(string_index_node)
-
-
-def get_text(rich_node):
-    """Read rich text, discarding formatting if not disallowed"""
-    text_node = rich_node.find('{%s}t' % SHEET_MAIN_NS)
-    text = text_node.text or unicode('')
-
-    if text_node.get('{%s}space' % XML_NS) != 'preserve':
-        text = text.strip()
-
-    # fix XML escaping sequence for '_x'
-    text = text.replace('x005F_', '')
-    return unicode(text)
