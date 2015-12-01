a7590f06763b2602c25dd44f0cd4f8eca2206604
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-12-01 15:08:28 +0100

    Switch to using an encoded string to keep Jenkins happy.

--- openpyxl2/cell/tests/test_text.py
+++ openpyxl2/cell/tests/test_text.py
@@ -102,9 +102,9 @@ class TestPhoneticText:
     def test_ctor(self, PhoneticText):
         text = PhoneticText(sb=9, eb=10, t=u"よ")
         xml = tostring(text.to_tree())
-        expected = u"""
+        expected = b"""
         <rPh sb="9" eb="10">
-            <t>よ</t>
+            <t>\xe3\x82\x88</t>
         </rPh>
         """
         diff = compare_xml(xml, expected)
@@ -112,9 +112,9 @@ class TestPhoneticText:
 
 
     def test_from_xml(self, PhoneticText):
-        src = u"""
+        src = b"""
         <rPh sb="9" eb="10">
-            <t>よ</t>
+            <t>\xe3\x82\x88</t>
         </rPh>
         """
         node = fromstring(src)
