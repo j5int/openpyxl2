4bd51fa0d1033085511758fcd6796b447a980f18
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-01-29 21:08:56 +0100

    Correct reference.

--- doc/filters.py
+++ doc/filters.py
@@ -26,6 +26,6 @@ for r in data:
 
 ws.auto_filter.ref = "A1:B15"
 ws.auto_filter.add_filter_column(0, ["Kiwi", "Apple", "Mango"])
-ws.auto_filter.add_sort_condition("B2:15")
+ws.auto_filter.add_sort_condition("B2:B15")
 
 wb.save("filtered.xlsx")
