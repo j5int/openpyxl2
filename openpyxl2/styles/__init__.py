a8bcb1e2c8ea63460055001a7fa968d84d708cd0
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-02-07 17:46:30 +0100

    Remove unused imports and private class from public API.

--- openpyxl2/styles/__init__.py
+++ openpyxl2/styles/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2016 openpyxl
 
-from openpyxl2.descriptors import Typed
-from openpyxl2.descriptors.serialisable import Serialisable
 
 from .alignment import Alignment
 from .borders import Border, Side
@@ -11,4 +9,3 @@ from .fills import PatternFill, GradientFill, Fill
 from .fonts import Font, DEFAULT_FONT
 from .numbers import NumberFormatDescriptor, is_date_format, is_builtin
 from .protection import Protection
-from .proxy import StyleProxy
