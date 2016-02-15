797cf8cd9049babd875843a938f4938392be8f50
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-02-15 20:58:29 +0100

    Index the range at 1 to avoid counting.

--- openpyxl2/worksheet/worksheet.py
+++ openpyxl2/worksheet/worksheet.py
@@ -720,8 +720,8 @@ class Worksheet(_WorkbookChild):
         if self.min_row == self.max_row == self.min_column == self.max_column:
             return ((),)
         cols = []
-        for col_idx in range(self.max_column):
-            cells = self.get_squared_range(col_idx + 1, self.min_row, col_idx + 1, self.max_row)
+        for col_idx in range(1, self.max_column+1):
+            cells = self.get_squared_range(col_idx, self.min_row, col_idx, self.max_row)
             col = chain.from_iterable(cells)
             cols.append(tuple(col))
         return tuple(cols)
