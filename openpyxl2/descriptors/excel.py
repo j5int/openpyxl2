f10a8b58019c54d012308ec95dca2f96259f19ce
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-02-20 19:08:33 +0100

    Make pattern slightly more restrictive.

--- openpyxl2/descriptors/excel.py
+++ openpyxl2/descriptors/excel.py
@@ -84,7 +84,7 @@ class Guid(MatchPattern):
 
 class CellRange(MatchPattern):
 
-    pattern = "^{0}$".format(RANGE_EXPR)
+    pattern = """^[$]?(?P<min_col>[A-Za-z]{1,3})[$]?(?P<min_row>\d+)(:[$]?(?P<max_col>[A-Za-z]{1,3})[$]?(?P<max_row>\d+)?)?$"""
     allow_none = True
 
     def __set__(self, instance, value):
