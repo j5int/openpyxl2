d3102d466717eb0d3eb49859177ecf142fbfab7d
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-04-05 13:53:38 +0200

    Add Python 3.5 to the classifiers See #619

--- setup.py
+++ setup.py
@@ -74,5 +74,6 @@ setup(name='openpyxl2',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
                  ],
     )
