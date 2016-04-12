f2a81ae1720de9536357e976607f9946dd7ac200
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-04-12 11:45:56 +0200

    Remove unused import.

--- openpyxl2/formatting/formatting.py
+++ openpyxl2/formatting/formatting.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 # Copyright (c) 2010-2016 openpyxl
 
 from openpyxl2.compat import OrderedDict, deprecated
-from openpyxl2.styles.differential import DifferentialStyle
 from .rule import Rule
 
 
