91234e4f1fb66997af0abc68167c028cbce83e28
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-04-17 13:19:03 +0200

    Refactor tests.

--- openpyxl2/worksheet/tests/test_worksheet_copy.py
+++ openpyxl2/worksheet/tests/test_worksheet_copy.py
@@ -4,39 +4,23 @@ from __future__ import absolute_import
 import pytest
 
 from openpyxl2 import Workbook, load_workbook
-from openpyxl2.comments import Comment
-from openpyxl2.styles.cell_style import StyleArray
-from openpyxl2.styles import PatternFill
-from openpyxl2.worksheet.dimensions import RowDimension, ColumnDimension
-from openpyxl2.worksheet.copier import WorksheetCopy
-
 
 def compare_cells(source_cell, target_cell):
-    attrs = ('_value', 'data_type', 'value', 'row', 'col_idx', '_comment',
-             '_hyperlink', '_style')
+    attrs = ('_value', 'data_type', '_comment', '_hyperlink', '_style')
 
     for attr in attrs:
         s_attr = getattr(source_cell, attr)
         o_attr = getattr(target_cell, attr)
-        if (s_attr is not None and o_attr is not None):
-            if s_attr != o_attr:
-                return False
-        elif s_attr is None and o_attr is None:
-            pass
-        else: return False
+        if s_attr != o_attr:
+            return False
     return True
 
 
+@pytest.fixture
+def WorksheetCopy():
+    from ..copier import WorksheetCopy
 
-@pytest.fixture()
-def load_copy_worksheets(datadir):
-    datadir.chdir()
-    wb = load_workbook('copy_test.xlsx')
-    ws1 = wb['original_sheet']
-    ws2 = wb.create_sheet('copy_sheet')
-    cp = WorksheetCopy(ws1, ws2)
-    cp.copy_worksheet()
-    return ws1, ws2
+    return WorksheetCopy
 
 
 @pytest.fixture()
@@ -47,33 +31,44 @@ def copy_worksheets():
     return ws1, ws2
 
 
-def test_copy_between_workbooks():
-    wb1 = Workbook()
-    ws1 = wb1.active
-    wb2 = Workbook()
-    ws2 = wb2.active
-    with pytest.raises(ValueError):
-        WorksheetCopy(ws1, ws2)
+class TestWorksheetCopy:
+
+    def test_ctor(self, copy_worksheets, WorksheetCopy):
+        ws1, ws2 = copy_worksheets
+        copier = WorksheetCopy(ws1, ws2)
+        assert copier.source_worksheet == ws1
+        assert copier.target_worksheet == ws2
 
 
-def test_cannot_copy_to_self(copy_worksheets):
-    ws1, ws2 = copy_worksheets
-    with pytest.raises(ValueError):
-        WorksheetCopy(ws1, ws1)
+    def test_cannot_copy_between_workbooks(self, WorksheetCopy):
+        wb1 = Workbook()
+        ws1 = wb1.active
+        wb2 = Workbook()
+        ws2 = wb2.active
+        with pytest.raises(ValueError):
+            WorksheetCopy(ws1, ws2)
 
 
-def test_merged_cell_copy(copy_worksheets):
-    ws1, ws2 = copy_worksheets
-    ws1.merge_cells('A10:A11')
-    ws1.merge_cells('F20:J23')
-    WorksheetCopy(ws1, ws2).copy_worksheet()
-    assert ws1.merged_cell_ranges == ws2.merged_cell_ranges
+    def test_cannot_copy_to_self(self, WorksheetCopy, copy_worksheets):
+        ws1, ws2 = copy_worksheets
+        with pytest.raises(ValueError):
+            WorksheetCopy(ws1, ws1)
 
 
-def test_merged_cell_copy_change(copy_worksheets):
-    ws1, ws2 = copy_worksheets
-    ws1.merge_cells('A10:A11')
-    ws1.merge_cells('F20:J23')
-    WorksheetCopy(ws1, ws2).copy_worksheet()
-    ws2._merged_cells[1] = 'F2:F3'
-    assert ws1.merged_cell_ranges != ws2.merged_cell_ranges
+    def test_merged_cell_copy(self, WorksheetCopy, copy_worksheets):
+        ws1, ws2 = copy_worksheets
+        ws1.merge_cells('A10:A11')
+        ws1.merge_cells('F20:J23')
+        WorksheetCopy(ws1, ws2).copy_worksheet()
+        assert ws1.merged_cell_ranges == ws2.merged_cell_ranges
+
+
+@pytest.fixture()
+def load_copy_worksheets(datadir, WorksheetCopy):
+    datadir.chdir()
+    wb = load_workbook('copy_test.xlsx')
+    ws1 = wb['original_sheet']
+    ws2 = wb.create_sheet('copy_sheet')
+    cp = WorksheetCopy(ws1, ws2)
+    cp.copy_worksheet()
+    return ws1, ws2
