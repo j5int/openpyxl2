835634e7ea7194531a77c48d947ab3bc1c698649
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-04-17 13:38:32 +0200

    Add tests for copying cells.

--- openpyxl2/worksheet/tests/test_worksheet_copy.py
+++ openpyxl2/worksheet/tests/test_worksheet_copy.py
@@ -4,6 +4,9 @@ from __future__ import absolute_import
 import pytest
 
 from openpyxl2 import Workbook, load_workbook
+from openpyxl2.comments import Comment
+from openpyxl2.styles import Font
+
 
 def compare_cells(source_cell, target_cell):
     attrs = ('_value', 'data_type', '_comment', '_hyperlink', '_style')
@@ -24,17 +27,19 @@ def WorksheetCopy():
 
 
 @pytest.fixture()
-def copy_worksheets():
+def copier(WorksheetCopy):
     wb = Workbook()
     ws1 = wb.active
     ws2 = wb.create_sheet('copy_sheet')
-    return ws1, ws2
+    return WorksheetCopy(ws1, ws2)
 
 
 class TestWorksheetCopy:
 
-    def test_ctor(self, copy_worksheets, WorksheetCopy):
-        ws1, ws2 = copy_worksheets
+    def test_ctor(self, WorksheetCopy):
+        wb = Workbook()
+        ws1 = wb.create_sheet()
+        ws2 = wb.create_sheet()
         copier = WorksheetCopy(ws1, ws2)
         assert copier.source_worksheet == ws1
         assert copier.target_worksheet == ws2
@@ -49,18 +54,55 @@ class TestWorksheetCopy:
             WorksheetCopy(ws1, ws2)
 
 
-    def test_cannot_copy_to_self(self, WorksheetCopy, copy_worksheets):
-        ws1, ws2 = copy_worksheets
+    def test_cannot_copy_to_self(self, WorksheetCopy):
+        wb1 = Workbook()
+        ws1 = wb1.active
         with pytest.raises(ValueError):
             WorksheetCopy(ws1, ws1)
 
 
-    def test_merged_cell_copy(self, WorksheetCopy, copy_worksheets):
-        ws1, ws2 = copy_worksheets
+    def test_merged_cell_copy(self, copier):
+        ws1 = copier.source_worksheet
+        ws2 = copier.target_worksheet
         ws1.merge_cells('A10:A11')
         ws1.merge_cells('F20:J23')
-        WorksheetCopy(ws1, ws2).copy_worksheet()
+        copier.copy_worksheet()
         assert ws1.merged_cell_ranges == ws2.merged_cell_ranges
+
+
+    def test_cell_copy_value(self, copier):
+        ws1 = copier.source_worksheet
+        ws2 = copier.target_worksheet
+        ws1['A1'] = 4
+        copier.copy_cells()
+        assert ws2['A1'].value == 4
+
+
+    def test_cell_copy_style(self, copier):
+        ws1 = copier.source_worksheet
+        ws2 = copier.target_worksheet
+        c1 = ws1['A1']
+        c1.font = Font(bold=True)
+        copier.copy_cells()
+        assert ws2['A1'].font == Font(bold=True)
+
+
+    def test_cell_copy_comment(self, copier):
+        ws1 = copier.source_worksheet
+        ws2 = copier.target_worksheet
+        c1 = ws1['A1']
+        c1.comment = Comment("A Comment", "Nobody")
+        copier.copy_cells()
+        assert ws2['A1'].comment == Comment("A Comment", "Nobody")
+
+
+    def test_cell_copy_hyperlink(self, copier):
+        ws1 = copier.source_worksheet
+        ws2 = copier.target_worksheet
+        c1 = ws1['A1']
+        c1.hyperlink = "http://www.example.com"
+        copier.copy_cells()
+        assert ws2['A1'].hyperlink.target == "http://www.example.com"
 
 
 @pytest.fixture()
