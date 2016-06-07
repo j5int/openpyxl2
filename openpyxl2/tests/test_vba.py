be440b095bb99c416e4de9a440a1f6efe4aca42f
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-06-07 10:04:08 +0200

    Correct code for checking whether content type has been added. Reenable skipped test.

--- openpyxl2/tests/test_vba.py
+++ openpyxl2/tests/test_vba.py
@@ -16,7 +16,6 @@ from openpyxl2.xml.functions import fromstring, tostring
 from openpyxl2.xml.constants import SHEET_MAIN_NS, REL_NS, CONTYPES_NS
 
 
-@pytest.mark.xfail
 def test_content_types(datadir):
     datadir.join('reader').chdir()
     fname = 'vba+comments.xlsm'
