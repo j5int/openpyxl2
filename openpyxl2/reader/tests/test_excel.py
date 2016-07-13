056a1aa554c332b265c7294d97d93d6e1ac14203
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-07-13 12:20:27 +0200

    Test for disabling external links.

--- openpyxl2/reader/tests/test_excel.py
+++ openpyxl2/reader/tests/test_excel.py
@@ -80,3 +80,11 @@ def test_load_workbook_with_vba(datadir):
         wb2 = load_workbook(BytesIO(f.read()), keep_vba=True)
     assert wb1.vba_archive.namelist() == wb2.vba_archive.namelist()
     assert wb1.vba_archive.namelist() == ZipFile(test_file, 'r').namelist()
+
+
+def test_no_external_links(datadir):
+    from ..excel import load_workbook
+
+    datadir.chdir()
+    wb = load_workbook("bug137.xlsx", keep_links=False)
+    assert wb.keep_links is False
