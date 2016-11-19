cc7ad9b0a817daa08f4538c0cbd3bae510b0d2a5
Charlie Clark <charlie.clark@clark-consulting.eu>
2016-11-19 13:13:38 +0100

    Convert integration tests into unit tests.

--- openpyxl2/comments/tests/test_comment.py
+++ openpyxl2/comments/tests/test_comment.py
@@ -7,38 +7,39 @@ from openpyxl2.comments import Comment
 from openpyxl2.workbook import Workbook
 from openpyxl2.worksheet import Worksheet
 
+import pytest
 
-def test_init():
-    wb = Workbook()
-    ws = Worksheet(wb)
-    c = Comment("text", "author")
-    ws.cell(coordinate="A1").comment = c
-    assert c._parent is ws.cell(coordinate="A1")
-    assert c.text == "text"
-    assert c.author == "author"
+@pytest.fixture
+def Comment():
+    from ..comments import Comment
+    return Comment
 
 
-def test_can_copy():
-    """A comment can be copied to another cell"""
-    wb = Workbook()
-    ws = Worksheet(wb)
-    comment = Comment("text", "author")
-    c1 =  ws.cell(row=1, column=1)
-    c1.comment = comment
-    c2 = ws.cell(row=2, column=1)
-    c2.comment = copy(comment)
+class TestComment:
 
-    assert c2.comment.parent is c2
-    assert c2.comment.text == "text"
-    assert c2.comment.author == "author"
+    def test_ctor(self, Comment):
+        comment = Comment(author="Charlie", text="A comment")
+        assert comment.author == "Charlie"
+        assert comment.text == "A comment"
+        assert comment.parent is None
 
 
-def test_can_remove():
-    """A comment can be removed"""
-    wb = Workbook()
-    ws = Worksheet(wb)
-    comment = Comment("text", "author")
-    cell = ws.cell(row=1, column=1)
-    cell.comment = comment
-    cell.comment = None
-    assert cell.comment is None
+    def test_bind(self, Comment):
+        comment = Comment("", "")
+        comment.bind("ws")
+        assert comment.parent == "ws"
+
+
+    def test_unbind(self, Comment):
+        comment = Comment("", "")
+        comment.bind("ws")
+        comment.unbind()
+        assert comment.parent is None
+
+
+    def test_copy(self, Comment):
+        comment = Comment("", "")
+        clone = copy(comment)
+        assert clone is not comment
+        assert comment.text == clone.text
+        assert comment.author == clone.author
