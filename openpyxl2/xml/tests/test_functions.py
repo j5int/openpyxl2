791335c5e5c15177de8a6af0fd61eee39931333f
Charlie Clark <charlie.clark@clark-consulting.eu>
2017-01-17 12:23:06 +0100

    Disable entity resolution of the XML parser Resolves #749

--- openpyxl2/xml/tests/test_functions.py
+++ openpyxl2/xml/tests/test_functions.py
@@ -1,5 +1,5 @@
 import pytest
-
+import xml
 
 def test_safe_iterator_none():
     from .. functions import safe_iterator
@@ -18,3 +18,26 @@ def test_localtag(xml, tag):
     from .. functions import fromstring
     node = fromstring(xml)
     assert localname(node) == tag
+
+
+@pytest.mark.lxml_required
+def test_dont_resolve():
+    from ..functions import fromstring
+    s = b"""<?xml version="1.0" encoding="ISO-8859-1"?>
+            <!DOCTYPE foo [
+            <!ELEMENT foo ANY >
+            <!ENTITY xxe SYSTEM "file:///dev/random" >]>
+            <foo>&xxe;</foo>"""
+    node = fromstring(s)
+
+
+@pytest.mark.no_lxml
+def test_dont_resolve():
+    from ..functions import fromstring
+    s = b"""<?xml version="1.0" encoding="ISO-8859-1"?>
+            <!DOCTYPE foo [
+            <!ELEMENT foo ANY >
+            <!ENTITY xxe SYSTEM "file:///dev/random" >]>
+            <foo>&xxe;</foo>"""
+    with pytest.raises(xml.etree.ElementTree.ParseError):
+        node = fromstring(s)
