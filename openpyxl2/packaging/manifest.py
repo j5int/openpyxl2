a58ff412e89c5b010ef39a6e58818c42855c5161
Vincent Barbaresi <vincent.barbaresi@gmail.com>
2017-02-08 15:00:27 +0100

    Check if mimetypes was already inited.
    This avoids a Python bug when mimetypes is inited more than once:
    https://bugs.python.org/issue4963

--- openpyxl2/packaging/manifest.py
+++ openpyxl2/packaging/manifest.py
@@ -37,7 +37,8 @@ from openpyxl2.xml.constants import (
 from openpyxl2.xml.functions import tostring
 
 # initialise mime-types
-mimetypes.init()
+if not mimetypes.inited:
+    mimetypes.init()
 mimetypes.add_type('application/xml', ".xml")
 mimetypes.add_type('application/vnd.openxmlformats-package.relationships+xml', ".rels")
 mimetypes.add_type("application/vnd.ms-office.vbaProject", ".bin")
