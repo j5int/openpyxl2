3331f106cc7d21bd6bd2055c80787f2726661b1e
Charlie Clark <charlie.clark@clark-consulting.eu>
2017-02-18 14:39:23 +0100

    Initial import of Laurent's code. Slightly modified: let's focus initially on the cells.

---
+++ openpyxl2/worksheet/tests/test_cell_range.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import
+# Copyright (c) 2010-2017 openpyxl
+import pytest
+
+
+@pytest.fixture
+def CellRange():
+    from ..cell_range import CellRange
+    return CellRange
+
+
+class TestCellRange:
+
+
+    def test_ctor(self, CellRange):
+        cr = CellRange(min_col=1, min_row=1, max_col=5, max_row=7)
+        assert cr.coord == "A1:E7"
+
+
+    def test_from_string(self, CellRange):
+        cr = CellRange.from_string("$A$1:B4")
+        assert cr.coord == "A1:B4"
+
+
+    def test_bottom(self, CellRange):
+        pass
+
+
+    def test_collapse(self, CellRange):
+        pass
+
+
+
+    def test_coord(self, CellRange):
+        pass
+
+
+    def test_expand(self, CellRange):
+        pass
+
+
+    def test_from_string(self, CellRange):
+        pass
+
+
+    def test_get_size(self, CellRange):
+        pass
+
+
+    def test_intersection(self, CellRange):
+        pass
+
+
+    def test_isdisjoint(self, CellRange):
+        pass
+
+
+    def test_issubset(self, CellRange):
+        pass
+
+
+    def test_issuperset(self, CellRange):
+        pass
+
+
+    def test_left(self, CellRange):
+        pass
+
+
+    def test_right(self, CellRange):
+        pass
+
+
+    def test_shift(self, CellRange):
+        pass
+
+
+    def test_top(self, CellRange):
+        pass
+
+
+    def test_union(self, CellRange):
+        pass
