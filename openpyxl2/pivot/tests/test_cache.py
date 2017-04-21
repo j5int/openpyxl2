811a7bee8de3f5a2c162c4f573a42e21ac7fb448
Charlie Clark <charlie.clark@clark-consulting.eu>
2017-04-21 14:24:53 +0200

    Initial test for shared items.

--- openpyxl2/pivot/tests/test_cache.py
+++ openpyxl2/pivot/tests/test_cache.py
@@ -5,6 +5,9 @@ import pytest
 from openpyxl2.xml.functions import fromstring, tostring
 from openpyxl2.tests.helper import compare_xml
 
+from ..record import Text
+
+
 @pytest.fixture
 def CacheField():
     from ..cache import CacheField
@@ -30,3 +33,40 @@ class TestCacheField:
         node = fromstring(src)
         field = CacheField.from_tree(node)
         assert field == CacheField(name="ID")
+
+
+@pytest.fixture
+def SharedItems():
+    from ..cache import SharedItems
+    return SharedItems
+
+
+class TestSharedItems:
+
+    def test_ctor(self, SharedItems):
+        s = [Text(v="Stanford"), Text(v="Cal"), Text(v="UCLA")]
+        items = SharedItems(s=s)
+        xml = tostring(items.to_tree())
+        expected = """
+        <sharedItems count="3">
+          <s b="0" i="0" st="0" un="0" v="Stanford"/>
+          <s b="0" i="0" st="0" un="0" v="Cal"/>
+          <s b="0" i="0" st="0" un="0" v="UCLA"/>
+        </sharedItems>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_xml(self, SharedItems):
+        src = """
+        <sharedItems count="3">
+          <s v="Stanford"></s>
+          <s v="Cal"></s>
+          <s v="UCLA"></s>
+        </sharedItems>
+        """
+        node = fromstring(src)
+        items = SharedItems.from_tree(node)
+        s = [Text(v="Stanford"), Text(v="Cal"), Text(v="UCLA")]
+        assert items == SharedItems(s=s)
