3e110f9bc636c2937382e7ecaff58aa31b7ca9e5
Austin Rogers <awrogers7@gmail.com>
2017-10-15 14:15:08 -0500

    Added unit test in test_read_only.py and sheet_get_row_test.xml for issue #908 bug fix.

--- openpyxl2/worksheet/tests/test_read_only.py
+++ openpyxl2/worksheet/tests/test_read_only.py
@@ -5,6 +5,7 @@ import pytest
 
 from openpyxl2.xml.functions import fromstring, tostring
 from openpyxl2.tests.helper import compare_xml
+from openpyxl2.cell import read_only
 
 
 class DummyWorkbook:
@@ -29,3 +30,39 @@ class TestReadOnlyWorksheet:
         cells = tuple(ws.get_squared_range(1, 1, 1, 1))
         assert len(cells) == 1
         assert cells[0][0].value == "col1"
+
+    def test_get_row(self, datadir, ReadOnlyWorksheet, Workbook):
+
+        datadir.chdir()
+
+        ws = ReadOnlyWorksheet(Workbook(), "Sheet", "", "sheet_get_row_test.xml", [])
+
+        # 1 non-empty cell, 1 empty cell
+        row0 = ws["B2:C2"][0]
+
+        assert len(row0) == 2
+        assert isinstance(row0[0], read_only.ReadOnlyCell) and row0[0].value == 2200
+        assert isinstance(row0[1], read_only.EmptyCell) and row0[1].value is None
+
+        # 2 non-empty cells, 2 empty cells
+        row1 = ws["A2:D2"][0]
+
+        assert len(row1) == 4
+        assert isinstance(row1[0], read_only.ReadOnlyCell) and row1[0].value == 1200
+        assert isinstance(row1[1], read_only.ReadOnlyCell) and row1[1].value == 2200
+        assert isinstance(row1[2], read_only.EmptyCell) and row1[2].value is None
+        assert isinstance(row1[3], read_only.EmptyCell) and row1[3].value is None
+
+        # 2 non-empty cells
+        row2 = ws["A2:B2"][0]
+
+        assert len(row2) == 2
+        assert isinstance(row2[0], read_only.ReadOnlyCell) and row2[0].value == 1200
+        assert isinstance(row2[1], read_only.ReadOnlyCell) and row2[1].value == 2200
+
+        # 2 empty cells
+        row3 = ws["C2:D2"][0]
+
+        assert len(row3) == 2
+        assert isinstance(row3[0], read_only.EmptyCell) and row3[0].value is None
+        assert isinstance(row3[1], read_only.EmptyCell) and row3[1].value is None
