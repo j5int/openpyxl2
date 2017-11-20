f614b5812344e88b3b3b94f036fe0d916e3f6975
Charlie Clark <charlie.clark@clark-consulting.eu>
2017-11-20 20:31:10 +0100

    Add simple test for LightRig and correct definition

--- openpyxl2/drawing/tests/test_shapes.py
+++ openpyxl2/drawing/tests/test_shapes.py
@@ -86,3 +86,57 @@ class TestCamera:
         node = fromstring(src)
         cam = Camera.from_tree(node)
         assert cam == Camera(prst="orthographicFront")
+
+
+@pytest.fixture
+def LightRig():
+    from ..shapes import LightRig
+    return LightRig
+
+
+class TestLightRig:
+
+    def test_ctor(self, LightRig):
+        rig = LightRig(rig="threePt", dir="t")
+        xml = tostring(rig.to_tree())
+        expected = """
+        <lightRig rig="threePt" dir="t"/>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_xml(self, LightRig):
+        src = """
+        <lightRig rig="threePt" dir="t"/>
+        """
+        node = fromstring(src)
+        rig = LightRig.from_tree(node)
+        assert rig == LightRig(rig="threePt", dir="t")
+
+
+@pytest.fixture
+def Bevel():
+    from ..shapes import Bevel
+    return Bevel
+
+
+class TestBevel:
+
+    def test_ctor(self, Bevel):
+        bevel = Bevel(w=10, h=20)
+        xml = tostring(bevel.to_tree())
+        expected = """
+        <bevel w="10" h="20"/>
+        """
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+
+    def test_from_xml(self, Bevel):
+        src = """
+        <bevel w="101600" h="101600"/>
+        """
+        node = fromstring(src)
+        bevel = Bevel.from_tree(node)
+        assert bevel == Bevel( w=101600, h=101600)
