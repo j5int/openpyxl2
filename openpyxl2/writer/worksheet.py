25dbfc14c5ede870bb1da6fcb6b084d0c65de89f
Magnus Schieder <mschieder@intevation.de>
2018-07-04 13:11:48 +0200

    Changes .itervalues() to .items()

--- openpyxl2/writer/worksheet.py
+++ openpyxl2/writer/worksheet.py
@@ -105,7 +105,7 @@ def write_worksheet(worksheet):
                 xf.write(cols)
 
             # The borders of all merged cells are formatted.
-            for mcr in ws._merged_cell_range.itervalues():
+            for _, mcr in ws._merged_cell_range.items():
                 mcr.format()
 
             # write data
