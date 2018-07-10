1acb80582647b298e616e71d76d5e9be0d02d478
Ben Webb <ben.webb@opendataservices.coop>
2018-07-10 17:40:04 +0100

    Call from_excel with an offset in worksheet openpyxl.reader.worksheet

--- openpyxl2/reader/tests/test_worksheet.py
+++ openpyxl2/reader/tests/test_worksheet.py
@@ -16,6 +16,7 @@ from openpyxl2.utils.indexed_list import IndexedList
 from openpyxl2.worksheet import Worksheet
 from openpyxl2.worksheet.pagebreak import Break, PageBreak
 from openpyxl2.packaging.relationship import Relationship, RelationshipList
+from openpyxl2.utils.datetime  import CALENDAR_WINDOWS_1900
 
 
 def test_get_xml_iter():
@@ -76,6 +77,7 @@ def Workbook():
         data_only = False
         _colors = []
         encoding = "utf8"
+        excel_base_date = CALENDAR_WINDOWS_1900
 
         def __init__(self):
             self._differential_styles = []
