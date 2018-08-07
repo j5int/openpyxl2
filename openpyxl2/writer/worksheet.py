39e7f9327979c1860a1761a47cfe7c18db9b2700
Update ignore <devnull@localhost>
2018-08-07 17:23:48 +0200

    Move towards using the WorksheetWriter to write worksheet XML.

--- openpyxl2/writer/worksheet.py
+++ openpyxl2/writer/worksheet.py
@@ -81,95 +81,16 @@ def write_worksheet(worksheet):
     """Write a worksheet to an xml file."""
 
     ws = worksheet
-    ws._rels = RelationshipList()
-    ws._hyperlinks = []
 
-    out = BytesIO()
-
-    with xmlfile(out) as xf:
-        with xf.element('worksheet', xmlns=SHEET_MAIN_NS):
-
-            props = ws.sheet_properties.to_tree()
-            xf.write(props)
-
-            dim = SheetDimension(ref=ws.calculate_dimension())
-            xf.write(dim.to_tree())
-
-            xf.write(ws.views.to_tree())
-
-            cols = ws.column_dimensions.to_tree()
-            ws.sheet_format.outlineLevelCol = ws.column_dimensions.max_outline
-            xf.write(ws.sheet_format.to_tree())
-
-            if cols is not None:
-                xf.write(cols)
-
-            # The borders of all merged cells are formatted.
-            for mcr in ws._merged_cell_range.values():
-                mcr.format()
-
-            # write data
-            write_rows(xf, ws)
-
-            if ws.protection:
-                xf.write(ws.protection.to_tree())
-
-            if ws.auto_filter:
-                xf.write(ws.auto_filter.to_tree())
-
-
-            merge = write_mergecells(ws)
-            if merge is not None:
-                xf.write(merge)
-
-            cfs = write_conditional_formatting(ws)
-            for cf in cfs:
-                xf.write(cf)
-
-            if ws.data_validations:
-                xf.write(ws.data_validations.to_tree())
-
-            hyper = write_hyperlinks(ws)
-            if hyper:
-                xf.write(hyper.to_tree())
-
-            options = ws.print_options
-            if dict(options):
-                new_element = options.to_tree()
-                xf.write(new_element)
-
-            margins = ws.page_margins.to_tree()
-            xf.write(margins)
-
-            setup = ws.page_setup
-            if dict(setup):
-                new_element = setup.to_tree()
-                xf.write(new_element)
-
-            if bool(ws.HeaderFooter):
-                xf.write(ws.HeaderFooter.to_tree())
-
-            if ws.page_breaks:
-                xf.write(ws.page_breaks.to_tree())
-
-            drawing = write_drawing(ws)
-            if drawing is not None:
-                xf.write(drawing)
-
-            # if there is an existing vml file associated with this sheet or if there
-            # are any comments we need to add a legacyDrawing relation to the vml file.
-            if (ws.legacy_drawing is not None or ws._comments):
-                legacyDrawing = Related(id="anysvml")
-                xml = legacyDrawing.to_tree("legacyDrawing")
-                xf.write(xml)
-
-            tables = _add_table_headers(ws)
-            if tables:
-                xf.write(tables.to_tree())
-
-    xml = out.getvalue()
-    out.close()
-    return xml
+    from openpyxl2.worksheet.writer import WorksheetWriter
+    writer = WorksheetWriter(ws)
+    writer.write_top()
+    writer.write_rows()
+    writer.write_tail()
+    writer.xf.close()
+    ws._rels = writer._rels
+    ws._hyperlinks = writer._hyperlinks
+    return writer.out.getvalue()
 
 
 def _add_table_headers(ws):
