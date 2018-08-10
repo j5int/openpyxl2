6789b056a7e4ed98f1366e7a0912535716e5640a
Update ignore <devnull@localhost>
2018-08-10 18:10:26 +0200

    Put shared code into a function.

--- openpyxl2/writer/cell.py
+++ openpyxl2/writer/cell.py
@@ -8,15 +8,17 @@ from openpyxl2.utils.datetime import to_excel, days_to_time
 from datetime import timedelta
 
 
-def etree_write_cell(xf, worksheet, cell, styled=None):
-
+def _set_attributes(cell, styled=None):
+    """
+    Set coordinate and datatype
+    """
     coordinate = cell.coordinate
-    attributes = {'r': coordinate}
+    attrs = {'r': coordinate}
     if styled:
-        attributes['s'] = '%d' % cell.style_id
+        attrs['s'] = '%d' % cell.style_id
 
     if cell.data_type != 'f':
-        attributes['t'] = cell.data_type
+        attrs['t'] = cell.data_type
 
     value = cell._value
 
@@ -26,8 +28,15 @@ def etree_write_cell(xf, worksheet, cell, styled=None):
                 value = days_to_time(value)
             value = value.isoformat()
         else:
-            attributes['t'] = "n"
-            value = to_excel(value, worksheet.parent.epoch)
+            attrs['t'] = "n"
+            value = to_excel(value, cell.parent.parent.epoch)
+
+    return value, attrs
+
+
+def etree_write_cell(xf, worksheet, cell, styled=None):
+
+    value, attributes = _set_attributes(cell, styled)
 
     el = Element("c", attributes)
     if value is None or value == "":
@@ -35,7 +44,7 @@ def etree_write_cell(xf, worksheet, cell, styled=None):
         return
 
     if cell.data_type == 'f':
-        shared_formula = worksheet.formula_attributes.get(coordinate, {})
+        shared_formula = worksheet.formula_attributes.get(cell.coordinate, {})
         formula = SubElement(el, 'f', shared_formula)
         if value is not None:
             formula.text = value[1:]
@@ -54,24 +63,7 @@ def etree_write_cell(xf, worksheet, cell, styled=None):
 
 
 def lxml_write_cell(xf, worksheet, cell, styled=False):
-    coordinate = cell.coordinate
-    attributes = {'r': coordinate}
-    if styled:
-        attributes['s'] = '%d' % cell.style_id
-
-    if cell.data_type != 'f':
-        attributes['t'] = cell.data_type
-
-    value = cell._value
-
-    if cell.data_type == "d":
-        if cell.parent.parent.iso_dates:
-            if isinstance(value, timedelta):
-                value = days_to_time(value)
-            value = value.isoformat()
-        else:
-            attributes['t'] = "n"
-            value = to_excel(value, worksheet.parent.epoch)
+    value, attributes = _set_attributes(cell, styled)
 
     if value == '' or value is None:
         with xf.element("c", attributes):
@@ -79,7 +71,7 @@ def lxml_write_cell(xf, worksheet, cell, styled=False):
 
     with xf.element('c', attributes):
         if cell.data_type == 'f':
-            shared_formula = worksheet.formula_attributes.get(coordinate, {})
+            shared_formula = worksheet.formula_attributes.get(cell.coordinate, {})
             with xf.element('f', shared_formula):
                 if value is not None:
                     xf.write(value[1:])
