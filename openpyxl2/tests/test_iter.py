afce6a6543e43db4fbe374e8faad893f888edf68
Charlie Clark <charlie.clark@clark-consulting.eu>
2018-10-22 19:22:16 +0200

    Move implementation-specific tests to worksheet module.

--- openpyxl2/tests/test_iter.py
+++ openpyxl2/tests/test_iter.py
@@ -322,140 +322,6 @@ def test_read_with_missing_cells(datadir, DummyWorkbook, ReadOnlyWorksheet):
     assert values == [1, 2, None, None, 3]
 
 
-def test_read_row(datadir, DummyWorkbook, ReadOnlyWorksheet):
-
-    src = b"""
-    <sheetData  xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" >
-    <row r="1" spans="4:27">
-      <c r="D1">
-        <v>1</v>
-      </c>
-      <c r="K1">
-        <v>0.01</v>
-      </c>
-      <c r="AA1">
-        <v>100</v>
-      </c>
-    </row>
-    </sheetData>
-    """
-
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
-
-    xml = fromstring(src)
-    row = tuple(ws._get_row(xml, 11, 11))
-    values = [c.value for c in row]
-    assert values == [0.01]
-
-    row = tuple(ws._get_row(xml, 1, 11))
-    values = [c.value for c in row]
-    assert values == [None, None, None, 1, None, None, None, None, None, None, 0.01]
-
-
-def test_read_empty_row(datadir, DummyWorkbook, ReadOnlyWorksheet):
-
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
-
-    src = """
-    <row r="2" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" />
-    """
-    element = fromstring(src)
-    row = ws._get_row(element, max_col=10)
-    row = tuple(row)
-    assert len(row) == 10
-
-
-def test_get_empty_cells_nonempty_row(datadir, DummyWorkbook, ReadOnlyWorksheet):
-    """Fix for issue #908.
-
-    Get row slice which only contains empty cells in a row containing non-empty
-    cells earlier in the row.
-    """
-
-    datadir.join("reader").chdir()
-
-    src = b"""
-    <sheetData  xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" >
-    <row r="1" spans="4:27">
-      <c r="A4">
-        <v>1</v>
-      </c>
-    </row>
-    </sheetData>
-    """
-
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
-
-    xml = fromstring(src)
-
-    min_col = 8
-    max_col = 9
-    row = tuple(ws._get_row(xml, min_col=min_col, max_col=max_col))
-
-    assert len(row) == 2
-    assert all(cell is EMPTY_CELL for cell in row)
-    values = [cell.value for cell in row]
-    assert values == [None, None]
-
-
-@pytest.mark.parametrize("row, column",
-                         [
-                             (2, 1),
-                             (3, 1),
-                             (5, 1),
-                         ]
-                         )
-def test_read_cell_from_empty_row(DummyWorkbook, ReadOnlyWorksheet, row, column):
-    src = BytesIO()
-    src.write(b"""<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-    <sheetData>
-      <row r="2" />
-      <row r="4" />
-    </sheetData>
-    </worksheet>
-    """)
-    src.seek(0)
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
-    ws._xml = src
-    cell = ws._get_cell(row, column)
-    assert cell is EMPTY_CELL
-
-
-def test_read_empty_rows(datadir, DummyWorkbook, ReadOnlyWorksheet):
-
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "empty_rows.xml", [])
-    rows = tuple(ws.rows)
-    assert len(rows) == 7
-
-
-def test_read_without_coordinates(DummyWorkbook, ReadOnlyWorksheet):
-
-    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", ["Whatever"]*10)
-    src = """
-    <row xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-      <c t="s">
-        <v>2</v>
-      </c>
-      <c t="s">
-        <v>4</v>
-      </c>
-      <c t="s">
-        <v>3</v>
-      </c>
-      <c t="s">
-        <v>6</v>
-      </c>
-      <c t="s">
-        <v>9</v>
-      </c>
-    </row>
-    """
-
-    element = fromstring(src)
-    row = tuple(ws._get_row(element, min_col=1, max_col=None, row_counter=1))
-    assert row[0].value == "Whatever"
-
-
 @pytest.mark.parametrize("read_only", [False, True])
 def test_read_empty_sheet(datadir, read_only):
     datadir.join("genuine").chdir()
@@ -470,3 +336,11 @@ def test_read_mac_date(datadir, read_only):
     wb = load_workbook("mac_date.xlsx", read_only=read_only)
     ws = wb.active
     assert ws['A1'].value == datetime.datetime(2016, 10, 3, 0, 0)
+
+
+def test_read_empty_rows(datadir, DummyWorkbook, ReadOnlyWorksheet):
+    datadir.join("reader").chdir()
+
+    ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "empty_rows.xml", [])
+    rows = tuple(ws.rows)
+    assert len(rows) == 7
--- openpyxl2/worksheet/tests/test_read_only.py
+++ openpyxl2/worksheet/tests/test_read_only.py
@@ -1,14 +1,25 @@
 from __future__ import absolute_import
 # Copyright (c) 2010-2018 openpyxl
 
+from io import BytesIO
+
 import pytest
 
+from openpyxl2.cell.read_only import EMPTY_CELL
+from openpyxl2.styles.styleable import StyleArray
+from openpyxl2.xml.functions import fromstring
 
-class DummyWorkbook:
+@pytest.fixture
+def DummyWorkbook():
+    class Workbook:
+        epoch = None
+        _cell_styles = [StyleArray([0, 0, 0, 0, 0, 0, 0, 0, 0])]
+        data_only = False
 
-    epoch = None
-    sheetnames = []
-    data_only = False
+        def __init__(self):
+            self.sheetnames = []
+
+    return Workbook()
 
 
 @pytest.fixture
@@ -27,3 +38,127 @@ class TestReadOnlyWorksheet:
         cells = tuple(ws.iter_rows(min_row=1, min_col=1, max_row=1, max_col=1))
         assert len(cells) == 1
         assert cells[0][0].value == "col1"
+
+
+    def test_read_row(self, DummyWorkbook, ReadOnlyWorksheet):
+
+        src = b"""
+        <sheetData  xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" >
+        <row r="1" spans="4:27">
+          <c r="D1">
+            <v>1</v>
+          </c>
+          <c r="K1">
+            <v>0.01</v>
+          </c>
+          <c r="AA1">
+            <v>100</v>
+          </c>
+        </row>
+        </sheetData>
+        """
+
+        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
+
+        xml = fromstring(src)
+        row = tuple(ws._get_row(xml, 11, 11))
+        values = [c.value for c in row]
+        assert values == [0.01]
+
+        row = tuple(ws._get_row(xml, 1, 11))
+        values = [c.value for c in row]
+        assert values == [None, None, None, 1, None, None, None, None, None, None, 0.01]
+
+
+    def test_read_empty_row(self, DummyWorkbook, ReadOnlyWorksheet):
+
+        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
+
+        src = """
+        <row r="2" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" />
+        """
+        element = fromstring(src)
+        row = ws._get_row(element, max_col=10)
+        row = tuple(row)
+        assert len(row) == 10
+
+
+    def test_get_empty_cells_nonempty_row(self, DummyWorkbook, ReadOnlyWorksheet):
+        """Fix for issue #908.
+
+        Get row slice which only contains empty cells in a row containing non-empty
+        cells earlier in the row.
+        """
+
+        src = b"""
+        <sheetData  xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" >
+        <row r="1" spans="4:27">
+          <c r="A4">
+            <v>1</v>
+          </c>
+        </row>
+        </sheetData>
+        """
+
+        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
+
+        xml = fromstring(src)
+
+        min_col = 8
+        max_col = 9
+        row = tuple(ws._get_row(xml, min_col=min_col, max_col=max_col))
+
+        assert len(row) == 2
+        assert all(cell is EMPTY_CELL for cell in row)
+        values = [cell.value for cell in row]
+        assert values == [None, None]
+
+
+
+    def test_read_without_coordinates(self, DummyWorkbook, ReadOnlyWorksheet):
+
+        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", ["Whatever"]*10)
+        src = """
+        <row xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+          <c t="s">
+            <v>2</v>
+          </c>
+          <c t="s">
+            <v>4</v>
+          </c>
+          <c t="s">
+            <v>3</v>
+          </c>
+          <c t="s">
+            <v>6</v>
+          </c>
+          <c t="s">
+            <v>9</v>
+          </c>
+        </row>
+        """
+
+        element = fromstring(src)
+        row = tuple(ws._get_row(element, min_col=1, max_col=None, row_counter=1))
+        assert row[0].value == "Whatever"
+
+    @pytest.mark.parametrize("row, column",
+                             [
+                                 (2, 1),
+                                 (3, 1),
+                                 (5, 1),
+                             ]
+                             )
+    def test_read_cell_from_empty_row(self, DummyWorkbook, ReadOnlyWorksheet, row, column):
+        src = b"""<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+        <sheetData>
+          <row r="2" />
+          <row r="4" />
+        </sheetData>
+        </worksheet>
+        """
+
+        ws = ReadOnlyWorksheet(DummyWorkbook, "Sheet", "", "", [])
+        ws._xml = BytesIO(src)
+        cell = ws._get_cell(row, column)
+        assert cell is EMPTY_CELL
